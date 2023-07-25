27928a35b76cc6d2123f0c190c6a001847c2f077
juanandreacchio <juan.andreacchio@gmail.com>
2023-07-25 15:49:45 -0300

    responsive

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const btnCambiarMode = document.querySelector('.dm-btn');
 const body = document.querySelector('body');
 let darkMode = false;
+const textoDarkMode = document.querySelector('.btn-text');
 desactivarDarkMode();
 
 function activarDarkMode(){
@@ -12,6 +13,18 @@ function desactivarDarkMode(){
     darkMode = false;
     body.classList.remove('dark-mode');
 }
+
+btnCambiarMode.addEventListener('mouseover',() =>{
+    if(darkMode){
+        textoDarkMode.classList.add('white-text')
+    }
+})
+
+btnCambiarMode.addEventListener('mouseout',() =>{
+    if(darkMode){
+        textoDarkMode.classList.remove('white-text')
+    }
+})
 
 btnCambiarMode.addEventListener('click',() => {
     if (darkMode){
